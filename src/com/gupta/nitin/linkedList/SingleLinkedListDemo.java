e2ca7af56924966a358e2cce1edadce3a3089c95
nitin <nitinguptamca@gmail.com>
2018-10-29 01:10:32 +0530

    add

---
+++ src/com/gupta/nitin/linkedList/SingleLinkedListDemo.java
@@ -0,0 +1,21 @@
+package com.gupta.nitin.linkedList;
+
+public class SingleLinkedListDemo {
+	private Node head, tail;
+	private int size;
+
+	private class Node {
+		int data;
+		Node next;
+
+		public Node(int data) {
+			this.data = data;
+		}
+	}
+
+	public static void main(String[] args) {
+		SingleLinkedListDemo demo = new SingleLinkedListDemo();
+
+	}
+
+}
