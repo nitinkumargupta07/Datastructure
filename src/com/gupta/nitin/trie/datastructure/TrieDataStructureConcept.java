baa26721fbd22e1cf324beaebc12c0fe358c494e
nitin <nitinguptamca@gmail.com>
2019-01-02 00:38:39 +0530

    update delete method

--- src/com/gupta/nitin/trie/datastructure/TrieDataStructureConcept.java
+++ src/com/gupta/nitin/trie/datastructure/TrieDataStructureConcept.java
@@ -84,6 +84,28 @@ public class TrieDataStructureConcept {
 			return result;
 	}
 
+	static private boolean delete(TrieNode current, String word, int index) {
+		if (index == word.length()) {
+			if (!current.bIsEnd) {
+				return false;
+			}
+			current.bIsEnd = false;
+			return current.children.isEmpty();
+		}
+		char ch = word.charAt(index);
+		TrieNode node = current.children.get(ch);
+		if (node == null) {
+			return false;
+		}
+		boolean shouldDeleteCurrentNode = delete(node, word, index + 1) && !node.bIsEnd;
+
+		if (shouldDeleteCurrentNode) {
+			current.children.remove(ch);
+			return current.children.isEmpty();
+		}
+		return false;
+	}
+
 	public static void main(String[] args) {
 		insert("are");
 		insert("area");
